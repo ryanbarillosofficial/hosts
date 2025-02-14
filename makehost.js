f366ddad99db75ac379eeff3a6e0b4b55163d7a7
ryanbarillosofficial <129530399+ryanbarillosofficial@users.noreply.github.com>
2025-02-14 00:48:11 -0700

    Trying test

--- makehost.js
+++ makehost.js
@@ -8,7 +8,7 @@ const BREAK_BLOCK = "\n\n\n";
 const BREAK_LINE = "#===============";
 const IP_PREFIX = {
   V4: "0.0.0.0",
-  V6: "::1",
+  V6: "::0",
 };
 
 export default function makehost(
@@ -51,18 +51,19 @@ export default function makehost(
     \n# https://github.com/ryanbarillos/hosts \
     \n#${BREAK_BLOCK}`;
 
-  // String of each domain blocked
+  // String of each domain blocked in IPv4 format
   const IPV4_ADDRESSES =
     `${BREAK_LINE}\n# IPv4 Addresses\n${BREAK_LINE}\n` +
     (() => {
       let output = "";
       for (let i = 0; i < DOMAINS_WITH_WWW_PREFIX.length; i++) {
-        output += `${IP_PREFIX.V4} ${DOMAINS_SANS_WWW_PREFIX[i]}\n`;
-        output += `${IP_PREFIX.V4} ${DOMAINS_WITH_WWW_PREFIX[i]}\n`;
+        output += `\n${IP_PREFIX.V4} ${DOMAINS_SANS_WWW_PREFIX[i]}`;
+        output += `\n${IP_PREFIX.V4} ${DOMAINS_WITH_WWW_PREFIX[i]}`;
       }
       return output;
     })() +
     BREAK_BLOCK;
+  // String of each domain blocked in IPv6 format
   const IPV6_ADDRESSES =
     `${BREAK_LINE}\n# IPv6 Addresses\n${BREAK_LINE}\n` +
     (() => {
@@ -75,15 +76,6 @@ export default function makehost(
     })();
 
   // Make the host text file
-  fs.writeFile(
-    FILE_PATH,
-    HOST_COMMENT + IPV4_ADDRESSES + IPV6_ADDRESSES,
-    (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Created \"${FILE_NAME}\" for \"${host_title}\" — yay!`);
-      }
-    }
-  );
+  fs.writeFileSync(FILE_PATH, HOST_COMMENT + IPV4_ADDRESSES + IPV6_ADDRESSES);
+  console.log(`Created \"${FILE_NAME}\" for \"${host_title}\" — yay!`);
 }
